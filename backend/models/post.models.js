842fc76517b9e7a47acabf184a365662b7bee381
Maxime <herymaxime@gmail.com>
2022-01-05 10:22:42 +0100

    correction bug backend

--- backend/models/post.models.js
+++ backend/models/post.models.js
@@ -2,22 +2,23 @@
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Post extends Model {
-    static associate(models) {
-      // define association here
-      models.Post.belongsTo(models.User, {
-        foreignKey: {
-          name: "idUsers",
-          allowNull: false,
-        },
-      });
-      models.Post.hasMany(models.Comment, {
-        foreignKey: {
-          name: "idPosts",
-          allowNull: false,
-        },
-      });
-    }
+    // static associate(models) {
+    //   // define association here
+    //   models.Post.belongsTo(models.User, {
+    //     foreignKey: {
+    //       name: "idUsers",
+    //       allowNull: false,
+    //     },
+    //   });
+    //   models.Post.hasMany(models.Comment, {
+    //     foreignKey: {
+    //       name: "idPosts",
+    //       allowNull: false,
+    //     },
+    //   });
+    // }
   }
+
   Post.init(
     {
       idUsers: DataTypes.INTEGER,
